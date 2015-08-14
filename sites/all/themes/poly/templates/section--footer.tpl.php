6c8e4ae76f47d0329f9d8835122cc4eddafd74ba
David Calcus <targoo@gmail.com>
2015-08-14 07:56:04 +0100

    fix mariqueclacus link

--- sites/all/themes/poly/templates/section--footer.tpl.php
+++ sites/all/themes/poly/templates/section--footer.tpl.php
@@ -2,7 +2,7 @@
   <?php print $content; ?>
   <div class="zone-wrapper zone-copyright-wrapper clearfix" id="zone-copyright-wrapper">
   <div class="zone zone-copyright clearfix container-12" id="zone-copyright">
-  &copy; <?php print date('Y')?> Polyclinique Borinage. Powered By <a href='www.mariquecalcus.com'>MARIQUECALCUS</a>
+  &copy; <?php print date('Y')?> Polyclinique Borinage. Powered By <a href='http://www.mariquecalcus.com'>MARIQUECALCUS</a>
   </div>
   </div>
 </footer>
